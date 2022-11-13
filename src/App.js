c5f144a1cd8f553aed8cba0dec02a2eda16be882
GovindVermagkv <sonygovind.gkv740@gmail.com>
2022-11-13 22:44:43 +0530

    first in branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
+<<<<<<< HEAD
 import PopulationChart from './Components/PopulationChart';
 import ExchangeRateChart from './Components/ExchangeRateChart';
+=======
+import './App.css';
+>>>>>>> 4c73cef (first in branch)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <PopulationChart />
       <ExchangeRateChart />
+=======
+      <>
+        <div>
+          <h1>worldometer dashboard</h1>
+        </div>
+      </>
+>>>>>>> 4c73cef (first in branch)
     </div>
   );
 }
