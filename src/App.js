737d6721ffdcdce8bc3a98ddfb88a8f589ce6541
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-14 13:10:29 +0530

    Linechart for Products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Donutchart from "./components/Donutchart";
+import Linechart from "./components/Linechart";
+// import { LineGraph } from "./components/LineGraph";
+
 
 function App() {
   return (
     <div>
       <Donutchart />
+      {/* <LineGraph /> */}
+      <Linechart />
     </div>
   );
 }
