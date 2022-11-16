6bca3b1a4f8d3ab7e4f9c49bb1c03dfcd4b967b7
puranchandralohar <clpuran95@gmail.com>
2022-11-16 10:50:45 +0530

    Added

--- src/components/ExchangeRateChart.jsx
+++ src/components/ExchangeRateChart.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import {
-  AreaChart,
+  LineChart,
   XAxis,
   YAxis,
-  Area,
+  Legend,
+  Line,
   CartesianGrid,
   Tooltip,
 } from "recharts";
 
 export default function ExchangeRateChart() {
   const [exchangeData, setExchangeData] = useState("");
-
 
   useEffect(() => {
     let url = "https://open.er-api.com/v6/latest/USD";
@@ -22,7 +22,6 @@ export default function ExchangeRateChart() {
     });
   }, []);
 
-
   const newData = [];
   Object.keys(exchangeData).forEach((ele) => {
     let temp = {};
@@ -30,48 +29,21 @@ export default function ExchangeRateChart() {
     temp.value = exchangeData[ele];
     newData.push(temp);
   });
-console.log("exchangeData=>>>",exchangeData)
-console.log("newData=>>>",newData)
 
   return (
     <>
       <div>
         <h1>Exchange Rates</h1>
-        <AreaChart
-          width={730}
-          height={250}
-          data={newData}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="name" />
-          <YAxis dataKey="value"/>
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="uv"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-          <Area
-            type="monotone"
-            dataKey="pv"
-            stroke="#82ca9d"
-            fillOpacity={1}
-            fill="url(#colorPv)"
-          />
-        </AreaChart>
+        <LineChart width={730} height={250} data={newData}
+  margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="name" />
+  <YAxis />
+  <Tooltip />
+  <Legend />
+  <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+  <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+</LineChart>
       </div>
     </>
   );
